213514e112d8180e26c48218a99d602be43140e9
Wes Parish <wes@elastiscale.net>
2021-05-28 19:57:00 +0000

    Adding verhash.dat download logic

--- amdgpu-pro-20.40/0.8.3/docker-entrypoint.sh
+++ amdgpu-pro-20.40/0.8.3/docker-entrypoint.sh
@@ -7,11 +7,20 @@
 #    PASSWORD=
 HOSTNAME=${HOSTNAME:-docker}
 
+DAT_LOCAL_PATH=/data/verthash.dat
+if [ "$ALGO" == "verthash" ]; then
+
+  NEXUS_MD5_SUM=$(curl -s https://nexus-jamie.elastiscale.net/service/rest/v1/search?repository=mining-repo | jq -r '.items[] | select(.name=="verthashminer/verthash.dat") | .assets[0].checksum.md5')
+  NEXUS_DL_URL=$(curl -s https://nexus-jamie.elastiscale.net/service/rest/v1/search?repository=mining-repo | jq -r '.items[] | select(.name=="verthashminer/verthash.dat") | .assets[0].downloadUrl')
+
+  mkdir -p /data
+  [ "$(md5sum $DAT_LOCAL_PATH | awk '{print $1}')" == "$NEXUS_MD5_SUM" ] || curl -L -o $DAT_LOCAL_PATH $NEXUS_DL_URL
+fi
 ARGS="-a $ALGO -o $POOL -u $WALLET_ADDRESS.$HOSTNAME --disable_colors"
 [ -n "$PASSWORD" ] && ARGS="$ARGS -p $PASSWORD"
 [ -n "$EXTRA_OPTS" ] && ARGS="$ARGS $EXTRA_OPTS"
 [ -n "$ETH_4G_MAX_ALLOC" ] && ARGS="$ARGS --eth_4g_max_alloc=$ETH_4G_MAX_ALLOC"
-[ -n "$VERTHASH_FILE" ] && ARGS="$ARGS --verthash_file=$VERTHASH_FILE"
+[ "$ALGO" == "verthash" ] && ARGS="$ARGS --verthash_file=/data/verthash.dat"
 
 COMMAND="/home/miner/teamredminer $ARGS"
 
